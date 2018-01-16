8a9e01489cea1b5722d9a36d3ffa608f63435424
hmueller01 <CuOckUpayW1d>
2018-01-16 22:43:26 +0100

    fixed error output of non double values, added more value types in OBIS output

--- examples/sml_server.c
+++ examples/sml_server.c
@@ -26,6 +26,7 @@
 #include <termios.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <sys/ioctl.h>
 
 #include <sml/sml_file.h>
@@ -85,16 +86,32 @@ void transport_receiver(unsigned char *buffer, size_t buffer_len) {
 	for (i = 0; i < file->messages_len; i++) {
 		sml_message *message = file->messages[i];
 		if (*message->message_body->tag == SML_MESSAGE_GET_LIST_RESPONSE) {
-			double value;
 			sml_list *entry;
 			sml_get_list_response *body;
 			body = (sml_get_list_response *) message->message_body->data;
 			for (entry = body->val_list; entry != NULL; entry = entry->next) {
-				value = sml_value_to_double(entry->value);
-				if (value != 0) {
+				if (entry->value->type == SML_TYPE_OCTET_STRING) {
+					// copy octet bytes to string
+					char *str = malloc(entry->value->data.bytes->len + 1);
+					memcpy(str, entry->value->data.bytes->str, entry->value->data.bytes->len);
+					str[entry->value->data.bytes->len] = 0;
+					printf("%d-%d:%d.%d.%d*%d#%x#\n",
+						entry->obj_name->str[0], entry->obj_name->str[1],
+						entry->obj_name->str[2], entry->obj_name->str[3],
+						entry->obj_name->str[4], entry->obj_name->str[5],
+						str);
+					free(str);
+				} else if (entry->value->type == SML_TYPE_BOOLEAN) {
+					printf("%d-%d:%d.%d.%d*%d#%s#\n",
+						entry->obj_name->str[0], entry->obj_name->str[1],
+						entry->obj_name->str[2], entry->obj_name->str[3],
+						entry->obj_name->str[4], entry->obj_name->str[5],
+						entry->value->data.boolean ? "true" : "false");
+				} else if (((entry->value->type & SML_TYPE_INTEGER) == SML_TYPE_INTEGER) ||
+						((entry->value->type & SML_TYPE_INTEGER) == SML_TYPE_INTEGER)) {
+					double value = sml_value_to_double(entry->value);
 					int scaler = (entry->scaler) ? *entry->scaler : 1;
-					if (scaler == -1)
-						value *= 0.1;
+					value = value * pow(10, scaler);
 					printf("%d-%d:%d.%d.%d*%d#%.1f#",
 						entry->obj_name->str[0], entry->obj_name->str[1],
 						entry->obj_name->str[2], entry->obj_name->str[3],
