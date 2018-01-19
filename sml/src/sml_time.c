84d58e687cf85890e8f319f258dd5c732b8c8363
hmueller01 <CuOckUpayW1d>
2018-01-19 19:50:23 +0100

    added comment for new meter type, corrected indentation

--- sml/src/sml_time.c
+++ sml/src/sml_time.c
@@ -52,13 +52,15 @@ sml_time *sml_time_parse(sml_buffer *buf) {
 	if (sml_buf_has_errors(buf)) goto error;
 
 	int type = sml_buf_get_next_type(buf);
-	switch(type) {
+	switch (type) {
 	case SML_TYPE_UNSIGNED:
 		tme->data.timestamp = sml_u32_parse(buf);
 		if (sml_buf_has_errors(buf)) goto error;
 		break;
 	case SML_TYPE_LIST:
-	{
+		// Some meters (e.g. FROETEC Multiflex ZG22) giving not one uint32
+		// as timestamp, but a list of 3 values.
+		// Ignoring these values, so that parsing does not fail.
 		sml_buf_get_next_length(buf); // should we check the length here?
 		u32 *t1 = sml_u32_parse(buf);
 		if (sml_buf_has_errors(buf)) goto error;
@@ -67,10 +69,9 @@ sml_time *sml_time_parse(sml_buffer *buf) {
 		i16 *t3 = sml_i16_parse(buf);
 		if (sml_buf_has_errors(buf)) goto error;
 		fprintf(stderr,
-				"libsml: error: sml_time as list[3]: ignoring value[0]=%u value[1]=%d value[2]=%d\n",
-				*t1, *t2, *t3);
-	}
-	break;
+			"libsml: error: sml_time as list[3]: ignoring value[0]=%u value[1]=%d value[2]=%d\n",
+			*t1, *t2, *t3);
+		break;
 	default:
 		goto error;
 	}
